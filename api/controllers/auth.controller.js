daccd56ed7eb894a9f94a341f3d255990e198261
hrushi1509 <hrushikeshwalujkar@gmail.com>
2023-11-26 11:10:31 +0530

    complete signin functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -25,7 +25,7 @@ export const signin = async(req,res,next) =>{
         }
         const validPassword = bcryptjs.compareSync(password,validUser.password);
         if(!validPassword){
-            next(401,'Wrong credentials!')
+            return next(401,'Wrong credentials!')
         }
 
         const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET,{})

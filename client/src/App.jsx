7c08d0be3cf5c59c049527ddd9a3f3b1fe5fdd1c
hrushi1509 <hrushikeshwalujkar@gmail.com>
2023-11-27 16:31:39 +0530

    update the header and make profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route, Navigate} from 'react-router-dom'
 import Home from './pages/Home'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 const App = () => {
   return (
     <BrowserRouter >
@@ -15,7 +16,12 @@ const App = () => {
          <Route path='/sign-in' element={<Signin/>}/>
          <Route path='/signup' element={<Signup/>}/>
          <Route path='/about' element={<About/>}/>
-         <Route path='/profile' element={<Profile/>}/>
+         <Route element={PrivateRoute}>
+            <Route path='/profile' element={<Profile/>}/>
+         </Route>
+         {/* <PrivateRoute path='/profile' element={<Profile />} /> */}
+         {/* <Route path='*' element={<Navigate to='/' />} /> */}
+
       </Routes>
     </BrowserRouter>
   )
---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import {Navigate, Outlet } from 'react-router-dom'
+
+export default function PrivateRoute() {
+  // const navigate = useNavigate()
+
+  const {currentUser} = useSelector(state => state.user)
+
+  return currentUser ? <Outlet/> : <Navigate to='/sign-in'/>
+            
+            
+        
+    
+}
+
+// export default PrivateRoute
